5468459784a4be68c563c394cc4656d41be9606a
StefanWaldraff <stefan.waldraff@gmail.com>
2014-05-23 00:12:39 +0200

    merged conflicting changes

--- src/de/htwg_konstanz/ebus/wholesaler/action/ImportAction.java
+++ src/de/htwg_konstanz/ebus/wholesaler/action/ImportAction.java
@@ -1,4 +1,3 @@
-<<<<<<< Upstream, based on origin/master
 /* LoginAction.java
 ***********************************************************************************
 * 15.03.2007 ** tdi
@@ -60,12 +59,12 @@ import de.htwg_konstanz.ebus.wholesaler.helper.ImportDOM;
 * @author tdi
 */
 public class ImportAction implements IAction  {
-	
+
 	public static final String ACTION_SHOW_PRODUCT_LIST = "showProductList";
 	public static final String PARAM_LOGIN_BEAN = "loginBean";
 	private static final String PARAM_PRODUCT_LIST = "productList";
 	private static final String UPLOAD_DIRECTORY = "upload";
-	
+
 	public ImportAction()
 	{
 		super();
@@ -81,21 +80,15 @@ public class ImportAction implements IAction  {
    */
 	public String execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> errorList)
 	{
-		
+
 		// Check that we have a file upload request
 		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
 		if(isMultipart)
 		{
-		
-		if (!ServletFileUpload.isMultipartContent(request)) {
-			
-			
-			System.out.println("LEER");
-			
-		}
-		
-		
-		
+
+
+
+
 		// configures upload settings
         DiskFileItemFactory factory = new DiskFileItemFactory();
 
@@ -106,17 +99,17 @@ public class ImportAction implements IAction  {
 
 		// Create a new file upload handler
 		ServletFileUpload upload = new ServletFileUpload(factory);
-		
-	
-		
+
+
+
 		// Parse the request
 			try {
 				List<FileItem> items = upload.parseRequest(request);
 				 if (items != null && items.size() > 0) {
 		                // iterates over form's fields
 					 for (FileItem item : items) {
-		               
-						 
+
+
 						 /*
 						// Process a regular form field
 						 if (item.isFormField()) {
@@ -134,11 +127,11 @@ public class ImportAction implements IAction  {
 							    long sizeInBytes = item.getSize();
 		                       
 		                         }*/
-		                        
+
 							    InputStream iSt = item.getInputStream();
 							    new ImportDOM(iSt);
 		            		 	System.out.println("I'm here");
-		                    
+
 					 }
 				 }
 				 else{
@@ -147,29 +140,29 @@ public class ImportAction implements IAction  {
 					 writer.flush();
 					 writer.close();
 				 }
-				 
-				 
-				 
+
+
+
 				 request.setAttribute("message","Upload has been done successfully!");
-				 
+
 			} 
 			catch (Exception ex) {
 	            request.setAttribute("message",
 	                    "There was an error: " + ex.getMessage());
 	            System.out.println("error: "+ ex.getMessage());
 			}
-			
-			
+
+
 		}
-			
-			 
-	    	
-		
-		
-		
+
+
+
+
+
+
 		// get the login bean from the session
 		LoginBean loginBean = (LoginBean)request.getSession(true).getAttribute(PARAM_LOGIN_BEAN);
-		
+
 		// ensure that the user is logged in
 		if (loginBean != null && loginBean.isLoggedIn())
 		{
@@ -181,17 +174,17 @@ public class ImportAction implements IAction  {
 				// find all available products and put it to the session
 				List<?> productList = ProductBOA.getInstance().findAll();
 				request.getSession(true).setAttribute(PARAM_PRODUCT_LIST, productList);					
-			    
+
 				// redirect to the import page
 				return "import.jsp";
-				
-			    
+
+
 			}
 			else
 			{
 				// authorization failed -> show error message
 				errorList.add("You are not allowed to perform this action!");
-				
+
 				// redirect to the welcome page
 				return "welcome.jsp";
 			}
@@ -214,220 +207,3 @@ public class ImportAction implements IAction  {
 		return actionName.equalsIgnoreCase(Constants.ACTION_IMPORT);
 	}
 }
-=======
-/* LoginAction.java
-***********************************************************************************
-* 15.03.2007 ** tdi
-* - created
-*
-***********************************************************************************
-* Copyright 2007 HTWG Konstanz
-* 
-* Prof. Dr.-Ing. Juergen Waesch
-* Dipl. -Inf. (FH) Thomas Dietrich
-* Fakultaet Informatik - Department of Computer Science
-* E-Business Technologien 
-* 
-* Hochschule Konstanz Technik, Wirtschaft und Gestaltung
-* University of Applied Sciences
-* Brauneggerstrasse 55
-* D-78462 Konstanz
-* 
-* E-Mail: juergen.waesch(at)htwg-konstanz.de
-************************************************************************************/
-package de.htwg_konstanz.ebus.wholesaler.action;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.jsp.PageContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
-
-import de.htwg_konstanz.ebus.framework.wholesaler.api.boa.ProductBOA;
-import de.htwg_konstanz.ebus.framework.wholesaler.api.security.Security;
-import de.htwg_konstanz.ebus.wholesaler.demo.IAction;
-import de.htwg_konstanz.ebus.wholesaler.demo.LoginBean;
-import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants;
-import de.htwg_konstanz.ebus.wholesaler.helper.ImportDOM;
-
-/**
-* The LoginAction processes an authentication request.<p>
-* The real work of the authentication process is done by the {@link LoginBean}.  
-*
-* @author tdi
-*/
-public class ImportAction implements IAction  {
-	
-	public static final String ACTION_SHOW_PRODUCT_LIST = "showProductList";
-	public static final String PARAM_LOGIN_BEAN = "loginBean";
-	private static final String PARAM_PRODUCT_LIST = "productList";
-	private static final String UPLOAD_DIRECTORY = "upload";
-	
-	public ImportAction()
-	{
-		super();
-	}
-
-   /**
-   * The execute method is automatically called by the dispatching sequence of the {@link ControllerServlet}. 
-   * 
-   * @param request the HttpServletRequest-Object provided by the servlet engine
-   * @param response the HttpServletResponse-Object provided by the servlet engine
-   * @param errorList a Stringlist for possible error messages occured in the corresponding action
-   * @return the redirection URL
-   */
-	public String execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> errorList)
-	{
-		
-		// Check that we have a file upload request
-		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-		if(isMultipart)
-		{
-		
-		if (!ServletFileUpload.isMultipartContent(request)) {
-			
-			
-			System.out.println("LEER");
-			
-		}
-		
-		
-		
-		// configures upload settings
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-
-		// Configure a repository (to ensure a secure temp location is used)
-		ServletContext servletContext = request.getSession().getServletContext();
-		File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
-		factory.setRepository(repository);
-
-		// Create a new file upload handler
-		ServletFileUpload upload = new ServletFileUpload(factory);
-		
-	
-		
-		// Parse the request
-			try {
-				List<FileItem> items = upload.parseRequest(request);
-				 if (items != null && items.size() > 0) {
-		                // iterates over form's fields
-					 for (FileItem item : items) {
-		               
-						 
-						 /*
-						// Process a regular form field
-						 if (item.isFormField()) {
-						     String name = item.getFieldName();
-						     String value = item.getString();
-						     
-						 }
-						 
-						 
-						 if (!item.isFormField()) {
-							 String fieldName = item.getFieldName();
-							    String fileName = item.getName();
-							    String contentType = item.getContentType();
-							    boolean isInMemory = item.isInMemory();
-							    long sizeInBytes = item.getSize();
-		                       
-		                         }*/
-		                        
-							    InputStream iSt = item.getInputStream();
-							    new ImportDOM(iSt);
-		            		 	System.out.println("I'm here");
-		                    
-					 }
-				 }
-				 else{
-					 PrintWriter writer =response.getWriter();
-					 writer.write("Keine Datei gewählt!");
-					 writer.flush();
-					 writer.close();
-				 }
-				 
-				 
-				 
-				 request.setAttribute("message","Upload has been done successfully!");
-				 
-			} 
-			catch (Exception ex) {
-	            request.setAttribute("message",
-	                    "There was an error: " + ex.getMessage());
-	            System.out.println("error: "+ ex.getMessage());
-			}
-			
-			
-		}
-			
-			 
-	    	
-		
-		
-		
-		// get the login bean from the session
-		LoginBean loginBean = (LoginBean)request.getSession(true).getAttribute(PARAM_LOGIN_BEAN);
-		
-		// ensure that the user is logged in
-		if (loginBean != null && loginBean.isLoggedIn())
-		{
-			// ensure that the user is allowed to execute this action (authorization)
-			// at this time the authorization is not fully implemented.
-			// -> use the "Security.RESOURCE_ALL" constant which includes all resources.
-			if (Security.getInstance().isUserAllowed(loginBean.getUser(), Security.RESOURCE_ALL, Security.ACTION_READ))
-			{
-				// find all available products and put it to the session
-				List<?> productList = ProductBOA.getInstance().findAll();
-				request.getSession(true).setAttribute(PARAM_PRODUCT_LIST, productList);					
-			    
-				// redirect to the import page
-				return "import.jsp";
-				
-			    
-			}
-			else
-			{
-				// authorization failed -> show error message
-				errorList.add("You are not allowed to perform this action!");
-				
-				// redirect to the welcome page
-				return "welcome.jsp";
-			}
-		}
-		else
-			// redirect to the login page
-			return "login.jsp";				
-	}
-
-   /**
-   * Each action itself decides if it is responsible to process the corrensponding request or not.
-   * This means that the {@link ControllerServlet} will ask each action by calling this method if it
-   * is able to process the incoming action request, or not.
-   * 
-   * @param actionName the name of the incoming action which should be processed
-   * @return true if the action is responsible, else false
-   */
-	public boolean accepts(String actionName)
-	{
-		return actionName.equalsIgnoreCase(Constants.ACTION_IMPORT);
-	}
-}
->>>>>>> 00c8752 moved ImportDOM.java from package action to helper because it isn't an action.
--- src/de/htwg_konstanz/ebus/wholesaler/helper/ImportDOM.java
+++ src/de/htwg_konstanz/ebus/wholesaler/helper/ImportDOM.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import java.lang.Object;
+import java.util.List;
 
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
@@ -20,82 +21,124 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import de.htwg_konstanz.ebus.framework.wholesaler.api.bo.BOProduct;
+import de.htwg_konstanz.ebus.framework.wholesaler.api.bo.BOSupplier;
+import de.htwg_konstanz.ebus.framework.wholesaler.api.boa.SupplierBOA;
+import de.htwg_konstanz.ebus.framework.wholesaler.vo.Supplier;
+import de.htwg_konstanz.ebus.framework.wholesaler.vo.util.Constants;
 
 public class ImportDOM {
-	
+
 	org.w3c.dom.Document dom;
-	
-	public ImportDOM(InputStream xml){
+
+	public ImportDOM(InputStream xml) {
 		try {
-		//Parsing of a XML-Document through Java
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		// configure Parsing Process with Factory 
-		dbf.setValidating(true);
-		dbf.setExpandEntityReferences(true);
-		//ignore Whitespaces
-		dbf.setIgnoringElementContentWhitespace(true);
-		dbf.setNamespaceAware(false);
-		//dbf.setSchema(null);
-		dbf.setIgnoringComments(true);
-		//dbf.setXIncludeAware(false);
-		
-		// Create a SchemaFactory capable of understanding W3C schemas
-		SchemaFactory factory = SchemaFactory.
-		newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-		Schema schema = factory.newSchema(new File("mySchema.xsd"));
-		// Create a Validator object, which can be used to validate
-		// an instance document.
-		Validator validator = schema.newValidator();
-		
-		
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		//db.isIgnoringElementContentWhitespace();
-		//TODO !!!!!db.setErrorHandler(org.xml.sax.helpers.DefaultHandler);
-		db.setEntityResolver(null);
-		org.w3c.dom.Document document = db.parse(xml);
-		// Validate the DOM tree
-		validator.validate(new DOMSource(document));
-			this.dom=document;
-	
+			// Parsing of a XML-Document through Java
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			// configure Parsing Process with Factory
+			dbf.setValidating(true);
+			dbf.setExpandEntityReferences(true);
+			// ignore Whitespaces
+			dbf.setIgnoringElementContentWhitespace(true);
+			dbf.setNamespaceAware(false);
+			dbf.setIgnoringComments(true);
+
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			dbf.setIgnoringElementContentWhitespace(true);
+
+			org.w3c.dom.Document document = db.parse(xml);
+
+			if (this.validate(document)) {
+				getSupplier(document);
+				writeToDatabase(getSupplier(document));
+			}
+
+			this.dom = document;
+
 		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		catch (SAXException e) {
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		}
-	
-	
-	//Validieren wsdl validiert
-	//DOM Element Childs ... 
-	//Datenbank verknüpfung
-	
-	public void writeToDatabase(){
-		
-	
-		
-		
 	}
-	
-	
-	public static void doSomething(Node node) {
-	    // do something with the current node instead of System.out
-	    System.out.println(node.getNodeName());
 
-	    NodeList nodeList = node.getChildNodes();
-	    for (int i = 0; i < nodeList.getLength(); i++) {
-	        Node currentNode = nodeList.item(i);
-	        if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
-	            //calls this method for all the children which is Element
-	            doSomething(currentNode);
-	        }
-	    }
-	
-	
-	
-	
+	// Validieren wsdl validiert
+	// DOM Element Childs ...
+	// Datenbank verknüpfung
+
+	public boolean validate(org.w3c.dom.Document document) {
+
+		// Create a SchemaFactory capable of understanding W3C schemas
+
+		System.out.println("TEST");
+		SchemaFactory factory = SchemaFactory
+				.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema;
+		try {
+			schema = factory
+					.newSchema(new File(
+							"C:\\Users\\Natalie\\Desktop\\Ebut\\bmecat_new_catalog_1_2_simple_without_NS.xsd"));
+
+			// Create a Validator object, which can be used to validate
+			// an instance document.
+			Validator validator = schema.newValidator();
+			// Validate the DOM tree
+			validator.validate(new DOMSource(document));
+			System.out.println("TRUE!");
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+
 	}
-	
+
+	// checken ob optional oder nicht bei xml
+	public BOSupplier getSupplier(org.w3c.dom.Document document) {
+
+		// test if Supplier is in Database
+		NodeList supplier = document.getElementsByTagName("SUPPLIER_NAME");
+		System.out.println(supplier.item(0).hasChildNodes());
+		String companyname = null;
+		BOSupplier endsupplier = null;
+
+		if (supplier.getLength() >= 1) {
+
+			companyname = supplier.item(0).getFirstChild().getNodeValue();
+			// TODO writeToDatabase(new
+			// BOSupplier().setCompanyname(companyname));
+
+		}
+
+		List<BOSupplier> suppliers = SupplierBOA.getInstance().findAll();
+		for (BOSupplier supp : suppliers) {
+
+			if (endsupplier.getCompanyname().equals(suppliers)) {
+
+				endsupplier = supp;
+			}
+		}
+		return endsupplier;
+	}
+
+	public void writeToDatabase(BOSupplier sup) {
+
+		SupplierBOA supboa = null;
+		supboa.saveOrUpdate(sup);
+
+	}
+
+	public void uploadFile() {
+
+	}
+
 }
