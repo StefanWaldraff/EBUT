5d059b095e369b3804509e6e95bd62c3a6f129cd
StefanWaldraff <stefan.waldraff@gmail.com>
2014-05-22 19:46:44 +0200

    Removed auto-generated TODO-tags. Please remove them next time BEFORE you check in.

--- src/de/htwg_konstanz/ebus/wholesaler/helper/ImportDOM.java
+++ src/de/htwg_konstanz/ebus/wholesaler/helper/ImportDOM.java
@@ -58,14 +58,11 @@ public class ImportDOM {
 			this.dom=document;
 	
 		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		catch (SAXException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		}
