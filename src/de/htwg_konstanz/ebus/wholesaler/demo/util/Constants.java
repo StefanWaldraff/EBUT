4de98e765435fa9f565995ad0adbf2eb3ac6e41d
StefanWaldraff <stefan.waldraff@gmail.com>
2014-05-22 23:46:13 +0200

    Divided Export in ExportXML and ExportXHTML

--- src/de/htwg_konstanz/ebus/wholesaler/demo/util/Constants.java
+++ src/de/htwg_konstanz/ebus/wholesaler/demo/util/Constants.java
@@ -23,7 +23,8 @@ public class Constants
 	public static final String ACTION_LOGIN = "login";
 	public static final String ACTION_LOGOUT = "logout";
 	public static final String ACTION_IMPORT = "importAction";
-	public static final String ACTION_EXPORT = "exportAction";
+	public static final String ACTION_EXPORT_XML = "exportXmlAction";
+	public static final String ACTION_EXPORT_XHTML = "exportXhtmlAction";
 
 	public static final String ACTION_READ_WS_SETTINGS = "readWsSettings";
 	public static final String ACTION_UPDATE_WS_SETTINGS = "updateWsSettings";
