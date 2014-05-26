3633f5622a8e059bdd88cbaf41e98b6c3e4804f0
StefanWaldraff <stefan.waldraff@gmail.com>
2014-05-26 07:00:21 +0200

    Moved functionality from ExportAction to ExportHandler

--- src/de/htwg_konstanz/ebus/wholesaler/action/ExportAction.java
+++ src/de/htwg_konstanz/ebus/wholesaler/action/ExportAction.java
@@ -20,23 +20,17 @@
  ************************************************************************************/
 package de.htwg_konstanz.ebus.wholesaler.action;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.w3c.dom.Document;
-
-import de.htwg_konstanz.ebus.framework.wholesaler.api.bo.BOProduct;
-import de.htwg_konstanz.ebus.framework.wholesaler.api.boa.ProductBOA;
 import de.htwg_konstanz.ebus.framework.wholesaler.api.security.Security;
 import de.htwg_konstanz.ebus.wholesaler.demo.ControllerServlet;
 import de.htwg_konstanz.ebus.wholesaler.demo.IAction;
 import de.htwg_konstanz.ebus.wholesaler.demo.LoginBean;
 import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants;
-import de.htwg_konstanz.ebus.wholesaler.main.dom.DomInteractor;
+import de.htwg_konstanz.ebus.wholesaler.main.handler.ExportHandler;
 
 /**
  * The LoginAction processes an authentication request.
@@ -46,8 +40,6 @@ import de.htwg_konstanz.ebus.wholesaler.main.dom.DomInteractor;
  * @author tdi
  */
 public class ExportAction implements IAction {
-
-	private List<String> errorList;
 
 	public ExportAction() {
 		super();
@@ -82,8 +74,7 @@ public class ExportAction implements IAction {
 			// resources.
 			if (Security.getInstance().isUserAllowed(loginBean.getUser(),
 					Security.RESOURCE_ALL, Security.ACTION_READ)) {
-				this.errorList = errorList;
-				handleExport(request, response);
+				new ExportHandler(errorList).process(request, response);
 				return "export.jsp";
 			} else {
 				// authorization failed -> show error message
@@ -109,66 +100,6 @@ public class ExportAction implements IAction {
 	public boolean accepts(String actionName) {
 		return actionName.equalsIgnoreCase(Constants.ACTION_EXPORT_XML)
 				|| actionName.equalsIgnoreCase(Constants.ACTION_EXPORT_XHTML);
-	}
-
-	private void handleExport(HttpServletRequest request,
-			HttpServletResponse response) {
-		if (request.getParameter("type") == null)
-			// only called by page loading -> no action
-			return;
-
-		String action = request.getParameter("action");
-		String match = request.getParameter("match");
-
-		List<BOProduct> productList = filterProductList(request, match);
-		if (productList.isEmpty()) {
-			// no products remaining after filtering -> error
-			errorList.add("No matching products found for pattern: " + match);
-		}
-		Document dom = null;
-		if (errorList.isEmpty())
-			dom = DomInteractor.createDomFromData(productList, errorList);
-		if (errorList.isEmpty())
-			DomInteractor.validateXml(dom, errorList);
-		File file = null;
-		if (errorList.isEmpty()) {
-			switch (action) {
-			case Constants.ACTION_EXPORT_XML:
-				file = DomInteractor.createFileFromDom(dom, "xml", errorList);
-				break;
-			case Constants.ACTION_EXPORT_XHTML:
-				// TODO transform XML DOM to XHTML DOM
-
-				file = DomInteractor.createFileFromDom(dom, "xhtml", errorList);
-				break;
-			}
-		}
-		if (errorList.isEmpty()) {
-			@SuppressWarnings("unchecked")
-			ArrayList<String> urls = (ArrayList<String>) request.getSession(
-					true).getAttribute("filesUploaded");
-			String filePath = file.getAbsolutePath();
-			urls.add(filePath);
-		}
-	}
-
-	private List<BOProduct> filterProductList(HttpServletRequest request,
-			String toMatch) {
-		List<BOProduct> productList = ProductBOA.getInstance().findAll();
-		if (toMatch == null || toMatch.equals(""))
-			// match is empty, nothing to filter
-			return productList;
-
-		// convert to lower case
-		toMatch = toMatch.toLowerCase();
-		List<BOProduct> filteredList = new ArrayList<BOProduct>();
-		for (BOProduct p : productList) {
-			String shortDescription = p.getShortDescription();
-			if (shortDescription != null
-					&& shortDescription.toLowerCase().contains(toMatch))
-				filteredList.add(p);
-		}
-		return filteredList;
 	}
 
 }
---
+++ src/de/htwg_konstanz/ebus/wholesaler/main/handler/ExportHandler.java
@@ -0,0 +1,82 @@
+package de.htwg_konstanz.ebus.wholesaler.main.handler;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.w3c.dom.Document;
+
+import de.htwg_konstanz.ebus.framework.wholesaler.api.bo.BOProduct;
+import de.htwg_konstanz.ebus.framework.wholesaler.api.boa.ProductBOA;
+import de.htwg_konstanz.ebus.wholesaler.demo.util.Constants;
+import de.htwg_konstanz.ebus.wholesaler.main.dom.DomInteractor;
+
+public class ExportHandler {
+	private final List<String> errorList;
+
+	public ExportHandler(List<String> errorList) {
+		this.errorList = errorList;
+	}
+
+	public void process(HttpServletRequest request, HttpServletResponse response) {
+		if (request.getParameter("type") == null)
+			// only called by page loading -> no action
+			return;
+
+		String action = request.getParameter("action");
+		String match = request.getParameter("match");
+
+		List<BOProduct> productList = filterProductList(request, match);
+		if (productList.isEmpty()) {
+			// no products remaining after filtering -> error
+			errorList.add("No matching products found for pattern: " + match);
+		}
+		Document dom = null;
+		if (errorList.isEmpty())
+			dom = DomInteractor.createDomFromData(productList, errorList);
+		if (errorList.isEmpty())
+			DomInteractor.validateXml(dom, errorList);
+		File file = null;
+		if (errorList.isEmpty()) {
+			switch (action) {
+			case Constants.ACTION_EXPORT_XML:
+				file = DomInteractor.createFileFromDom(dom, "xml", errorList);
+				break;
+			case Constants.ACTION_EXPORT_XHTML:
+				// TODO transform XML DOM to XHTML DOM
+
+				file = DomInteractor.createFileFromDom(dom, "xhtml", errorList);
+				break;
+			}
+		}
+		if (errorList.isEmpty()) {
+			@SuppressWarnings("unchecked")
+			ArrayList<String> urls = (ArrayList<String>) request.getSession(
+					true).getAttribute("filesUploaded");
+			String filePath = file.getAbsolutePath();
+			urls.add(filePath);
+		}
+	}
+
+	private List<BOProduct> filterProductList(HttpServletRequest request,
+			String toMatch) {
+		List<BOProduct> productList = ProductBOA.getInstance().findAll();
+		if (toMatch == null || toMatch.equals(""))
+			// match is empty, nothing to filter
+			return productList;
+
+		// convert to lower case
+		toMatch = toMatch.toLowerCase();
+		List<BOProduct> filteredList = new ArrayList<BOProduct>();
+		for (BOProduct p : productList) {
+			String shortDescription = p.getShortDescription();
+			if (shortDescription != null
+					&& shortDescription.toLowerCase().contains(toMatch))
+				filteredList.add(p);
+		}
+		return filteredList;
+	}
+}
